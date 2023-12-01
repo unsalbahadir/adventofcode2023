5e5554d88ca705e18bab88bae66d4c3ab8640158
unsalbahadir <bbahadirunsal@gmail.com>
2023-12-01 06:08:32 +0100

    Add Day1-1 solution

--- src/main/java/solutions/day1/Solution.java
+++ src/main/java/solutions/day1/Solution.java
@@ -8,9 +8,40 @@ import java.util.List;
 public class Solution {
 
     public int getSolution(List<String> lines) {
+        int sum = 0;
 
-        return 0;
+        for (String line : lines) {
+            String firstDigit = getFirstDigit(line);
+            String lastDigit = getLastDigit(line);
+
+            String numAsString = firstDigit + lastDigit;
+            int num = Integer.parseInt(numAsString);
+            sum += num;
+        }
+
+        return sum;
     }
+
+    private String getFirstDigit(String line) {
+        for (int i = 0; i < line.length(); i++) {
+            char c = line.charAt(i);
+            if (Character.isDigit(c)) {
+                return String.valueOf(c);
+            }
+        }
+        return "0";
+    }
+
+    private String getLastDigit(String line) {
+        for (int i = line.length() - 1; i >= 0; i--) {
+            char c = line.charAt(i);
+            if (Character.isDigit(c)) {
+                return String.valueOf(c);
+            }
+        }
+        return "0";
+    }
+
 
     public static void main(String[] args) throws IOException {
         Solution solution = new Solution();
