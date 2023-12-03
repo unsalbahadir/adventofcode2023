e204105f2782b8279ca7f657a993bdc41a127eb9
unsalbahadir <bbahadirunsal@gmail.com>
2023-12-03 06:58:40 +0100

    Add Day3-2 solution

--- src/main/java/solutions/day3/Solution.java
+++ src/main/java/solutions/day3/Solution.java
@@ -43,10 +43,6 @@ public class Solution {
             if (!Character.isDigit(c)) {
                 if (!currentNumber.isEmpty()) {
                     int value = Integer.parseInt(currentNumber.toString());
-//                    int characterBeforeNumber = currentNumberPositions.getFirst().column - 1;
-//                    if (characterBeforeNumber >= 0 && line.charAt(characterBeforeNumber) == '-') {
-//                        value = -value;
-//                    }
                     numbers.add(new Number(value, currentNumberPositions));
                     currentNumber = new StringBuilder();
                     currentNumberPositions = new ArrayList<>();
@@ -88,10 +84,97 @@ public class Solution {
         return !Character.isLetterOrDigit(character) && character != '.';
     }
 
+    public long getSolution2(List<String> lines) {
+        List<Number> numbers = new ArrayList<>();
+        Set<Position> gearPositions = new HashSet<>();
+        for (int i = 0; i < lines.size(); i++) {
+            String line = lines.get(i);
+            Pair<List<Number>, Set<Position>> result = parseLine2(line, i);
+            numbers.addAll(result.getLeft());
+            gearPositions.addAll(result.getRight());
+        }
+
+        return getGearRatioSum(numbers, gearPositions);
+    }
+
+    private Pair<List<Number>, Set<Position>> parseLine2(String line, int row) {
+        List<Number> numbers = new ArrayList<>();
+        Set<Position> gearPositions = new HashSet<>();
+
+        StringBuilder currentNumber = new StringBuilder();
+        List<Position> currentNumberPositions = new ArrayList<>();
+        for (int i = 0; i < line.length(); i++) {
+            char c = line.charAt(i);
+            if (!Character.isDigit(c)) {
+                if (!currentNumber.isEmpty()) {
+                    int value = Integer.parseInt(currentNumber.toString());
+                    numbers.add(new Number(value, currentNumberPositions));
+                    currentNumber = new StringBuilder();
+                    currentNumberPositions = new ArrayList<>();
+                }
+            } else {
+                currentNumber.append(c);
+                currentNumberPositions.add(new Position(row, i));
+            }
+            if (c == '*') {
+                gearPositions.add(new Position(row, i));
+            }
+        }
+        // add if number is at the end
+        if (!currentNumber.isEmpty()) {
+            numbers.add(new Number(Integer.parseInt(currentNumber.toString()), currentNumberPositions));
+        }
+        return Pair.of(numbers, gearPositions);
+    }
+
+    private long getGearRatioSum(List<Number> numbers, Set<Position> gearPositions) {
+        long result = 0;
+        for (Position gearPosition : gearPositions) {
+            List<Number> numbersAdjacentToGear = getNumbersAdjacentToGear(numbers, gearPosition);
+            if (numbersAdjacentToGear.size() == 2) {
+                long ratio = 1;
+                for (Number number : numbersAdjacentToGear) {
+                    ratio *= number.value;
+                }
+                result += ratio;
+            }
+        }
+
+        return result;
+    }
+
+    private List<Number> getNumbersAdjacentToGear(List<Number> numbers, Position gearPosition) {
+        List<Number> adjacentNumbers = new ArrayList<>();
+        for (Number number : numbers) {
+            if (isNumberAdjacentToGear(number, gearPosition)) {
+                adjacentNumbers.add(number);
+                if (adjacentNumbers.size() == 2) {
+                    break;
+                }
+            }
+        }
+        return adjacentNumbers;
+    }
+
+    private boolean isNumberAdjacentToGear(Number number, Position gearPosition) {
+        return number.positions.stream().anyMatch(position -> {
+            return new Position(position.row - 1, position.column - 1).equals(gearPosition) ||
+                    new Position(position.row - 1, position.column).equals(gearPosition) ||
+                    new Position(position.row - 1, position.column + 1).equals(gearPosition) ||
+                    new Position(position.row, position.column - 1).equals(gearPosition) ||
+                    new Position(position.row, position.column).equals(gearPosition) ||
+                    new Position(position.row, position.column + 1).equals(gearPosition) ||
+                    new Position(position.row + 1, position.column - 1).equals(gearPosition) ||
+                    new Position(position.row + 1, position.column).equals(gearPosition) ||
+                    new Position(position.row + 1, position.column + 1).equals(gearPosition);
+        });
+    }
+
     public static void main(String[] args) throws IOException {
         Solution solution = new Solution();
 
         List<String> lines = Files.readAllLines(Paths.get("inputs/day3.txt"));
         System.out.println(solution.getSolution(lines));
+        System.out.println(solution.getSolution2(lines));
     }
 }
