445195acbb90a6d6cd4726266e7cebb8e81168f8
unsalbahadir <bbahadirunsal@gmail.com>
2023-12-07 07:41:53 +0100

    Add Day7-2 solution

--- src/main/java/solutions/day7/Solution.java
+++ src/main/java/solutions/day7/Solution.java
@@ -3,10 +3,7 @@ package solutions.day7;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 public class Solution {
 
@@ -122,10 +119,118 @@ public class Solution {
         return 0;
     }
 
+    public long getSolution2(List<String> lines) {
+        List<Hand> hands = new ArrayList<>();
+        for (String line : lines) {
+            Hand hand = createHandWithJoker(line);
+            hands.add(hand);
+        }
+        List<Hand> sortedHands = sortHandsWithJoker(hands);
+
+        long result = 0;
+        long rank = 1;
+        for (Hand sortedHand : sortedHands) {
+            long winning = rank * sortedHand.bid;
+            result += winning;
+            rank++;
+        }
+
+        return result;
+    }
+
+    private Hand createHandWithJoker(String line) {
+        String[] split = line.split(" ");
+
+        String cards = split[0];
+        Type typeOfHand = getTypeOfHandWithJoker(cards);
+        Hand hand = new Hand(cards, Long.parseLong(split[1]), typeOfHand);
+
+        return hand;
+    }
+
+    private Type getTypeOfHandWithJoker(String cards) {
+        Map<Character, Integer> labelCounts = new HashMap<>();
+        for (int i = 0; i < cards.length(); i++) {
+            char c = cards.charAt(i);
+            labelCounts.merge(c, 1, Integer::sum);
+        }
+        if (labelCounts.containsKey('J') && labelCounts.size() > 1) {
+            Integer jokerCount = labelCounts.get('J');
+            labelCounts.remove('J');
+            Map.Entry<Character, Integer> entryWithHighestCount = getEntryWithHighestCount(labelCounts);
+            labelCounts.put(entryWithHighestCount.getKey(), entryWithHighestCount.getValue() + jokerCount);
+        }
+        Type type;
+        switch (labelCounts.size()) {
+            case 1 -> type = Type.FIVE_OF_A_KIND;
+            case 2 -> {
+                if (labelCounts.containsValue(4)) {
+                    type = Type.FOUR_OF_A_KIND;
+                } else {
+                    type = Type.FULL_HOUSE;
+                }
+            }
+            case 3 -> {
+                if (labelCounts.containsValue(3)) {
+                    type = Type.THREE_OF_A_KIND;
+                } else {
+                    type = Type.TWO_PAIR;
+                }
+            }
+            case 4 -> type = Type.ONE_PAIR;
+            default -> type = Type.HIGH_CARD;
+        }
+        return type;
+    }
+
+    private Map.Entry<Character, Integer> getEntryWithHighestCount(Map<Character, Integer> labelCounts) {
+        Map.Entry<Character, Integer> entry = Collections.max(labelCounts.entrySet(), Map.Entry.comparingByValue());
+        return entry;
+    }
+
+    private List<Hand> sortHandsWithJoker(List<Hand> hands) {
+        List<Hand> sortedHands = hands.stream().sorted(Solution::compareHandsWithJoker).toList();
+        return sortedHands;
+    }
+
+    private static int compareHandsWithJoker(Hand hand1, Hand hand2) {
+        Type type1 = hand1.type;
+        Type type2 = hand2.type;
+
+        if (type1 != type2) {
+            return -type1.compareTo(type2);
+        } else {
+            for (int i = 0; i < hand1.cards.length(); i++) {
+                char char1 = hand1.cards.charAt(i);
+                char char2 = hand2.cards.charAt(i);
+
+                if (char1 != char2) {
+                    if (Character.isDigit(char1) && Character.isDigit(char2)) {
+                        return Integer.compare(Character.getNumericValue(char1), Character.getNumericValue(char2));
+                    } else if (char1 == 'J') {
+                        return -1;
+                    } else if (char2 == 'J') {
+                        return 1;
+                    } else if (Character.isDigit(char1)) {
+                        return -1;
+                    } else if (Character.isDigit(char2)) {
+                        return 1;
+                    } else {
+                        LetterLabel letterLabel1 = LetterLabel.valueOf(String.valueOf(char1));
+                        LetterLabel letterLabel2 = LetterLabel.valueOf(String.valueOf(char2));
+                        return -letterLabel1.compareTo(letterLabel2);
+                    }
+                }
+            }
+        }
+        return 0;
+    }
+
     public static void main(String[] args) throws IOException {
         Solution solution = new Solution();
 
         List<String> lines = Files.readAllLines(Paths.get("inputs/day7.txt"));
         System.out.println(solution.getSolution(lines));
+        System.out.println(solution.getSolution2(lines));
     }
 }
