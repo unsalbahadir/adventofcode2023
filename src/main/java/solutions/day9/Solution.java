bf3003735f919bd4e89cc7bfa94be7e794963964
unsalbahadir <bbahadirunsal@gmail.com>
2023-12-09 06:36:11 +0100

    Add Day9-2 solution

--- src/main/java/solutions/day9/Solution.java
+++ src/main/java/solutions/day9/Solution.java
@@ -25,13 +25,39 @@ public class Solution {
             return 0;
         }
 
+        List<Integer> diffs = getDiffs(values);
+        int nextValue = getNextValue(diffs);
+        return values.getLast() + nextValue;
+    }
+
+    public int getSolution2(List<String> lines) {
+        int result = 0;
+        for (String line : lines) {
+            List<Integer> values = Arrays.stream(line.split(" ")).map(Integer::parseInt).toList();
+            int nextValue = getPreviousValue(values);
+            result += nextValue;
+        }
+
+        return result;
+    }
+
+    private int getPreviousValue(List<Integer> values) {
+        if (values.stream().allMatch(integer -> integer == 0)) {
+            return 0;
+        }
+
+        List<Integer> diffs = getDiffs(values);
+        int previousValue = getPreviousValue(diffs);
+        return values.getFirst() - previousValue;
+    }
+
+    private List<Integer> getDiffs(List<Integer> values) {
         List<Integer> diffs = new ArrayList<>();
         for (int i = 1; i < values.size(); i++) {
             int diff = values.get(i) - values.get(i - 1);
             diffs.add(diff);
         }
-        int nextValue = getNextValue(diffs);
-        return values.getLast() + nextValue;
+        return diffs;
     }
 
     public static void main(String[] args) throws IOException {
@@ -39,5 +65,6 @@ public class Solution {
 
         List<String> lines = Files.readAllLines(Paths.get("inputs/day9.txt"));
         System.out.println(solution.getSolution(lines));
+        System.out.println(solution.getSolution2(lines));
     }
 }
