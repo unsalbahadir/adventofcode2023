9c2ce438fdbebe839ada0162e301620e837c0286
unsalbahadir <bbahadirunsal@gmail.com>
2023-12-11 07:49:33 +0100

    Add Day11-2 solution

--- src/main/java/solutions/day11/Solution.java
+++ src/main/java/solutions/day11/Solution.java
@@ -9,21 +9,38 @@ import java.util.stream.Collectors;
 
 public class Solution {
 
-    record Position(int row, int column) {}
+    record Position(long row, long column) {}
 
-    public int getSolution(List<String> lines) {
+    public long getSolution(List<String> lines) {
         List<List<Character>> grid = convertToGrid(lines);
-        grid = expand(grid);
+        expand(grid);
 //        printGrid(grid);
         List<Position> galaxyPositions = findGalaxies(grid);
+//        for (Position galaxyPosition : galaxyPositions) {
+//            System.out.println(galaxyPosition);
+//        }
+        return getSumOfDistancesBetweenGalaxies(galaxyPositions);
+    }
+
+    public long getSolution2(List<String> lines) {
+        List<List<Character>> grid = convertToGrid(lines);
+        List<Position> galaxyPositions = findGalaxies(grid);
+//        System.out.println("Before expansion:");
+//        for (Position galaxyPosition : galaxyPositions) {
+//            System.out.println(galaxyPosition);
+//        }
+        expand2(grid, galaxyPositions, 1000000);
+//        System.out.println("After expansion:");
+//        for (Position galaxyPosition : galaxyPositions) {
+//            System.out.println(galaxyPosition);
+//        }
         return getSumOfDistancesBetweenGalaxies(galaxyPositions);
     }
 
     private List<List<Character>> convertToGrid(List<String> lines) {
         List<List<Character>> grid = new ArrayList<>();
 
-        for (int i = 0; i < lines.size(); i++) {
-            String line = lines.get(i);
+        for (String line : lines) {
             List<Character> row = new ArrayList<>();
             grid.add(row);
             for (int j = 0; j < line.length(); j++) {
@@ -33,7 +50,7 @@ public class Solution {
         return grid;
     }
 
-    private List<List<Character>> expand(List<List<Character>> grid) {
+    private void expand(List<List<Character>> grid) {
         List<Integer> rowsWithoutGalaxies = new ArrayList<>();
         for (int i = grid.size() - 1; i >= 0; i--) {
             List<Character> row = grid.get(i);
@@ -62,7 +79,46 @@ public class Solution {
                 characters.add(columnIndex, c);
             }
         }
-        return grid;
+    }
+
+    private void expand2(List<List<Character>> grid, List<Position> galaxyPositions, int expandFactor) {
+        List<Integer> rowsWithoutGalaxies = new ArrayList<>();
+        for (int i = grid.size() - 1; i >= 0; i--) {
+            List<Character> row = grid.get(i);
+            if (!containsGalaxy(row)) {
+                rowsWithoutGalaxies.add(i);
+            }
+        }
+
+        List<Integer> columnsWithoutGalaxies = new ArrayList<>();
+        for (int i = grid.size() - 1; i >= 0; i--) {
+            List<Character> column = getColumn(grid, i);
+            if (!containsGalaxy(column)) {
+                columnsWithoutGalaxies.add(i);
+            }
+        }
+
+        int linesToAdd = expandFactor - 1;
+
+        for (Integer rowIndex : rowsWithoutGalaxies) {
+            for (int i = 0; i < galaxyPositions.size(); i++) {
+                Position position = galaxyPositions.get(i);
+                if (position.row >= rowIndex) {
+                    Position newPosition = new Position(position.row + linesToAdd, position.column);
+                    galaxyPositions.set(i, newPosition);
+                }
+            }
+        }
+
+        for (Integer columnIndex : columnsWithoutGalaxies) {
+            for (int i = 0; i < galaxyPositions.size(); i++) {
+                Position position = galaxyPositions.get(i);
+                if (position.column >= columnIndex) {
+                    Position newPosition = new Position(position.row, position.column + linesToAdd);
+                    galaxyPositions.set(i, newPosition);
+                }
+            }
+        }
     }
 
     private List<Character> getColumn(List<List<Character>> grid, int columnIndex) {
@@ -90,13 +146,13 @@ public class Solution {
         return galaxyPositions;
     }
 
-    private int getSumOfDistancesBetweenGalaxies(List<Position> galaxyPositions) {
-        int sum = 0;
+    private long getSumOfDistancesBetweenGalaxies(List<Position> galaxyPositions) {
+        long sum = 0;
         for (int i = 0; i < galaxyPositions.size(); i++) {
             for (int j = i + 1; j < galaxyPositions.size(); j++) {
                 Position position1 = galaxyPositions.get(i);
                 Position position2 = galaxyPositions.get(j);
-                int distanceBetweenGalaxies = getDistanceBetweenGalaxies(position1, position2);
+                long distanceBetweenGalaxies = getDistanceBetweenGalaxies(position1, position2);
 //                System.out.println("Distance between " + (i + 1) + " (" + position1 + ")" + " and "
 //                        + (j + 1) + " (" + position2 + ")" + ": " + distanceBetweenGalaxies);
                 sum += distanceBetweenGalaxies;
@@ -105,7 +161,7 @@ public class Solution {
         return sum;
     }
 
-    private int getDistanceBetweenGalaxies(Position position1, Position position2) {
+    private long getDistanceBetweenGalaxies(Position position1, Position position2) {
         return Math.abs(position1.row - position2.row) + Math.abs(position1.column - position2.column);
     }
 
@@ -121,5 +177,6 @@ public class Solution {
 
         List<String> lines = Files.readAllLines(Paths.get("inputs/day11.txt"));
         System.out.println(solution.getSolution(lines));
+        System.out.println(solution.getSolution2(lines));
     }
 }
