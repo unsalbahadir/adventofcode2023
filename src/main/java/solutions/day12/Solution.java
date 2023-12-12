34e993185652e2f55a9edf296b2c33af2ffce358
unsalbahadir <bbahadirunsal@gmail.com>
2023-12-12 08:39:49 +0100

    Add unfold logic for Day11-2

--- src/main/java/solutions/day12/Solution.java
+++ src/main/java/solutions/day12/Solution.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class Solution {
 
@@ -27,6 +28,44 @@ public class Solution {
         return possibleArrangementCount;
     }
 
+    public int getSolution2(List<String> lines) {
+        int possibleArrangementCount = 0;
+
+        for (String line : lines) {
+            line = unfold(line);
+            String[] split = line.split(" ");
+            String springs = split[0];
+            List<Integer> damagedSpringGroups = Arrays.stream(split[1].split(","))
+                    .map(Integer::parseInt)
+                    .collect(Collectors.toList());
+            List<String> possibleArrangements = getPossibleArrangements(springs, damagedSpringGroups);
+            System.out.println("Possible arrangements size: " + possibleArrangements.size());
+            possibleArrangementCount += possibleArrangements.size();
+        }
+
+        return possibleArrangementCount;
+    }
+
+    private String unfold(String line) {
+        StringBuilder result = new StringBuilder();
+        String[] split = line.split(" ");
+
+        String springs = IntStream.range(0, 5)
+                .mapToObj(i -> split[0])
+                .collect(Collectors.joining("?"));
+
+        result.append(springs);
+        result.append(" ");
+
+        String damagedSpringGroups = IntStream.range(0, 5)
+                .mapToObj(i -> split[1])
+                .collect(Collectors.joining(","));
+        result.append(damagedSpringGroups);
+
+        System.out.println("Unfolded: " + result);
+        return result.toString();
+    }
+
     private List<String> getPossibleArrangements(String springs, List<Integer> damagedSpringGroups) {
         List<String> possibleArrangements = new ArrayList<>();
         tryPossibleArrangements(springs, damagedSpringGroups, possibleArrangements);
@@ -34,13 +73,13 @@ public class Solution {
         return possibleArrangements;
     }
 
-    private List<String> tryPossibleArrangements(String springs, List<Integer> damagedSpringGroups, List<String> results) {
+    private void tryPossibleArrangements(String springs, List<Integer> damagedSpringGroups, List<String> results) {
         int unknownCharIndex = springs.indexOf("?");
         if (unknownCharIndex == -1) {
             if (isValid(springs, damagedSpringGroups)) {
                 results.add(springs);
             }
-            return results;
+            return;
         }
 
         String withEmpty = replaceCharacter(springs, unknownCharIndex, '.');
@@ -48,8 +87,6 @@ public class Solution {
 
         String withFilled = replaceCharacter(springs, unknownCharIndex, '#');
         tryPossibleArrangements(withFilled, damagedSpringGroups, results);
-
-        return results;
     }
 
     private String replaceCharacter(String s, int index, char newChar) {
@@ -104,5 +141,6 @@ public class Solution {
 
         List<String> lines = Files.readAllLines(Paths.get("inputs/day12.txt"));
         System.out.println(solution.getSolution(lines));
+        System.out.println(solution.getSolution2(lines));
     }
 }
