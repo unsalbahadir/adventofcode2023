260181bdefc6e935221f60b6687ed4c4299651f3
unsalbahadir <bbahadirunsal@gmail.com>
2023-12-13 08:28:33 +0100

    Add Day13-2 solution

--- src/main/java/solutions/day13/Solution.java
+++ src/main/java/solutions/day13/Solution.java
@@ -8,6 +8,8 @@ import java.util.List;
 
 public class Solution {
 
+    boolean fixSmudge = false;
+
     public long getSolution(List<String> lines) {
         long result = 0;
 
@@ -64,9 +66,17 @@ public class Solution {
     }
 
     private long isHorizontalReflection(List<String> pattern) {
-        List<Integer> startingPoint = getStartingPoints(pattern);
+        if (fixSmudge) {
+            return isHorizontalReflectionWithSmudge(pattern);
+        } else {
+            return isHorizontalReflectionWithoutSmudge(pattern);
+        }
+    }
 
-        for (Integer i : startingPoint) {
+    private long isHorizontalReflectionWithoutSmudge(List<String> pattern) {
+        List<Integer> startingPointWithoutSmudge = getStartingPointsWithoutSmudge(pattern);
+
+        for (Integer i : startingPointWithoutSmudge) {
             if (isStartingPointValid(pattern, i)) {
                 return i;
             }
@@ -89,7 +99,53 @@ public class Solution {
         return true;
     }
 
-    private List<Integer> getStartingPoints(List<String> pattern) {
+    private long isHorizontalReflectionWithSmudge(List<String> pattern) {
+        long existingHorizontalReflection = isHorizontalReflectionWithoutSmudge(pattern);
+        List<Integer> startingPointWithoutSmudge = getStartingPointsWithoutSmudge(pattern).stream()
+                .filter(integer -> integer != existingHorizontalReflection)
+                .toList();
+        for (Integer i : startingPointWithoutSmudge) {
+            if (isStartingPointValidWithSmudge(pattern, i)) {
+                return i;
+            }
+        }
+
+        List<Integer> startingPointsWithSmudge = getStartingPointsWithSmudge(pattern);
+        for (Integer i : startingPointsWithSmudge) {
+            if (isStartingPointValidWithSmudge(pattern, i)) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
+    private boolean isStartingPointValidWithSmudge(List<String> pattern, int startingPoint) {
+        int left = startingPoint - 1;
+        int right = startingPoint;
+
+        boolean smudgeRemoved = false;
+        while (left >= 0 && right < pattern.size()) {
+            String line1 = pattern.get(left);
+            String line2 = pattern.get(right);
+            if (!line1.equals(line2)) {
+                if (smudgeRemoved) { // already removed one smudge
+                    return false;
+                }
+                int smudge = findSmudge(line1, line2);
+                if (smudge == -1) {
+                    return false;
+                }
+                smudgeRemoved = true;
+            }
+            left--;
+            right++;
+        }
+
+        return true;
+    }
+
+    private List<Integer> getStartingPointsWithoutSmudge(List<String> pattern) {
         List<Integer> startingPoints = new ArrayList<>();
         for (int i = 1; i < pattern.size(); i++) {
             if (pattern.get(i).equals(pattern.get(i - 1))) {
@@ -99,10 +155,45 @@ public class Solution {
         return startingPoints;
     }
 
+    private List<Integer> getStartingPointsWithSmudge(List<String> pattern) {
+        List<Integer> startingPoints = new ArrayList<>();
+        for (int i = 1; i < pattern.size(); i++) {
+            String line1 = pattern.get(i);
+            String line2 = pattern.get(i - 1);
+            int smudge = findSmudge(line1, line2);
+            if (smudge != -1) {
+                startingPoints.add(i);
+            }
+        }
+        return startingPoints;
+    }
+
+    private String replaceChar(String s, int index, char newChar) {
+        StringBuilder stringBuilder = new StringBuilder(s);
+        stringBuilder.setCharAt(index, newChar);
+        return stringBuilder.toString();
+    }
+
+    private int findSmudge(String line1, String line2) {
+        int diffCharIndex = -1;
+        for (int i = 0; i < line1.length(); i++) {
+            if (line1.charAt(i) != line2.charAt(i)) {
+                if (diffCharIndex != -1) {
+                    return -1;
+                } else {
+                    diffCharIndex = i;
+                }
+            }
+        }
+        return diffCharIndex;
+    }
+
     public static void main(String[] args) throws IOException {
         Solution solution = new Solution();
 
         List<String> lines = Files.readAllLines(Paths.get("inputs/day13.txt"));
+        System.out.println(solution.getSolution(lines));
+        solution.fixSmudge = true;
         System.out.println(solution.getSolution(lines));
     }
 }
