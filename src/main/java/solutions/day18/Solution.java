38c630c4027301df737e78142de8cafc1c1d0384
Bahadir Ünsal <bahadir.unsal@klm.com>
2023-12-18 11:49:17 +0100

    Add Day18-2 solution

--- src/main/java/solutions/day18/Solution.java
+++ src/main/java/solutions/day18/Solution.java
@@ -28,27 +28,7 @@ public class Solution {
         for (String line : lines) {
             doCommand(line, positions);
         }
-        Boundary boundary = getBoundary(positions);
-
-        System.out.println("Before dig:");
-        printGrid(positions, boundary);
-
-        positions = digInterior(positions, boundary);
-
-        System.out.println("After dig:");
-        printGrid(positions, boundary);
-
-        return positions.size();
-    }
-
-    public int getSolution2(List<String> lines) {
-        List<Position> positions = new ArrayList<>();
-        Position startingPosition = new Position(0, 0);
-        positions.add(startingPosition);
-
-        for (String line : lines) {
-            doCommand2(line, positions);
-        }
+//        System.out.println("Position count: " + positions.size());
         Boundary boundary = getBoundary(positions);
 
 //        System.out.println("Before dig:");
@@ -60,6 +40,28 @@ public class Solution {
 //        printGrid(positions, boundary);
 
         return positions.size();
+    }
+
+  public long getSolutionWithFormula(List<String> lines) {
+    List<Position> corners = new ArrayList<>();
+    Position startingPosition = new Position(0, 0);
+    corners.add(startingPosition);
+
+    for (String line : lines) {
+      addCorner(line, corners);
+    }
+    return getArea(corners);
+  }
+
+    public long getSolution2(List<String> lines) {
+        List<Position> corners = new ArrayList<>();
+        Position startingPosition = new Position(0, 0);
+        corners.add(startingPosition);
+
+        for (String line : lines) {
+            addCorner2(line, corners);
+        }
+        return getArea(corners);
     }
 
     private void doCommand(String line, List<Position> positions) {
@@ -75,6 +77,31 @@ public class Solution {
             }
         }
     }
+
+  private void addCorner(String line, List<Position> corners) {
+    String[] split = line.split(" ");
+    Direction direction = Direction.valueOf(split[0]);
+    int distance = Integer.parseInt(split[1]);
+
+    Position lastCorner = corners.getLast();
+    Position newPosition = getPositionInDirection(lastCorner, direction, distance);
+    corners.add(newPosition);
+  }
+
+  private void addCorner2(String line, List<Position> corners) {
+    String[] split = line.split(" ");
+    String hex = StringUtils.substringBetween(split[2], "#", ")");
+
+    Direction[] values = Direction.values();
+    int directionValue = Character.getNumericValue(hex.charAt(hex.length() - 1));
+    Direction direction = values[directionValue];
+
+    int distance = Integer.parseInt(hex.substring(0, 5), 16);
+
+    Position lastCorner = corners.getLast();
+    Position newPosition = getPositionInDirection(lastCorner, direction, distance);
+    corners.add(newPosition);
+  }
 
     private void doCommand2(String line, List<Position> positions) {
         String[] split = line.split(" ");
@@ -95,13 +122,20 @@ public class Solution {
         }
     }
 
-
     private Position getPositionInDirection(Position position, Direction direction) {
         return switch (direction) {
             case U -> new Position(position.row - 1, position.column);
             case R -> new Position(position.row, position.column + 1);
             case D -> new Position(position.row + 1, position.column);
             case L -> new Position(position.row, position.column - 1);
+        };
+    }
+    private Position getPositionInDirection(Position position, Direction direction, int distance) {
+        return switch (direction) {
+            case U -> new Position(position.row - distance, position.column);
+            case R -> new Position(position.row, position.column + distance);
+            case D -> new Position(position.row + distance, position.column);
+            case L -> new Position(position.row, position.column - distance);
         };
     }
 
@@ -117,6 +151,30 @@ public class Solution {
             minColumn = Math.min(minColumn, position.column);
         }
         return new Boundary(minRow, maxRow, minColumn, maxColumn);
+    }
+
+    private long getArea(List<Position> corners) {
+        long area = 0;
+        long circumference = 0;
+
+        for (int i = 0; i < corners.size(); i++) {
+            Position currentPosition = corners.get(i);
+
+            Position nextPosition;
+            if (i == corners.size() - 1) {
+              nextPosition = corners.get(0);
+            } else {
+              nextPosition = corners.get(i + 1);
+            }
+            circumference += Math.abs(nextPosition.row - currentPosition.row) + Math.abs(nextPosition.column - currentPosition.column);
+            area += (currentPosition.row * nextPosition.column) - (nextPosition.row * currentPosition.column);
+        }
+        System.out.println("Circumference:" + circumference);
+        long result = Math.abs(area) / 2;
+        System.out.println("Area before adding circumference: " + result);
+        result += circumference / 2;
+        result++;
+        return result;
     }
 
     private List<Position> digInterior(List<Position> positions, Boundary boundary) {
@@ -173,7 +231,16 @@ public class Solution {
         Solution solution = new Solution();
 
         List<String> lines = Files.readAllLines(Paths.get("inputs/day18.txt"));
-        System.out.println(solution.getSolution(lines));
-//        System.out.println(solution.getSolution2(lines));
+//        System.out.println(solution.getSolution(lines));
+//        System.out.println("With formula: " + solution.getSolutionWithFormula(lines));
+        System.out.println(solution.getSolution2(lines));
+
+//        List<Position> square = List.of(
+//            new Position(-2, -2),
+//            new Position(2, -2),
+//            new Position(2, 2),
+//            new Position(-2, 2)
+//        );
+//        System.out.println("Square area: " + solution.getArea(square));
     }
 }
