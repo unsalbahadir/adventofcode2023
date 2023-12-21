92907e929c16a94c3316adaaa38128df360db99c
unsalbahadir <bbahadirunsal@gmail.com>
2023-12-21 21:04:48 +0100

    Add Day21-2 cycle detection

--- src/main/java/solutions/day21/Solution.java
+++ src/main/java/solutions/day21/Solution.java
@@ -13,11 +13,14 @@ public class Solution {
     private record Position(int row, int column) {
     }
 
+    private record Farm(int row, int column, Set<Position> positions) {
+    }
+
     public int getSolution(List<String> lines) {
         List<Position> gardenPositions = getGardenPositions(lines);
         Position startingPosition = gardenPositions.getLast();
 
-        return countPossiblePositionsAfterSteps(new HashSet<>(gardenPositions), startingPosition, 10);
+        return countPossiblePositionsAfterSteps(new HashSet<>(gardenPositions), startingPosition, 500);
     }
 
     public int getSolution2(List<String> lines) {
@@ -53,18 +56,48 @@ public class Solution {
         Set<Position> visitingPositions = new HashSet<>();
         visitingPositions.add(startingPosition);
 
+        Set<Integer> possiblePositionCounts = new HashSet<>();
+        boolean cycleStarted = false;
+        Map<Boolean, Integer> results = new HashMap<>();
+
+        Map<Position, List<Position>> adjacentPositionsOfPosition = new HashMap<>();
+
         int steps = 0;
         while (steps < stepsToTake) {
             Set<Position> newVisitingPositions = new HashSet<>();
             for (Position visitingPosition : visitingPositions) {
-                List<Position> adjacentGardenPositions = getAdjacentGardenPositions(gardenPositions, visitingPosition);
+                List<Position> adjacentGardenPositions = adjacentPositionsOfPosition.get(visitingPosition);
+                if (adjacentGardenPositions == null) {
+                    adjacentGardenPositions = getAdjacentGardenPositions(gardenPositions, visitingPosition);
+                    adjacentPositionsOfPosition.put(visitingPosition, adjacentGardenPositions);
+                }
                 newVisitingPositions.addAll(adjacentGardenPositions);
             }
             visitingPositions = newVisitingPositions;
             steps++;
-        }
 
-//        System.out.println("Possible positions: " + visitingPositions);
+            int possiblePositionCount = visitingPositions.size();
+
+            // cycle detection
+            if (possiblePositionCounts.contains(possiblePositionCount)) {
+                // cycle starts
+                boolean isEven = steps % 2 == 0;
+                results.put(isEven, possiblePositionCount);
+                if (cycleStarted) {
+                    // have both results in the map now
+                    System.out.println("Cycle end found, results for isEven: " + results);
+                    return results.get(isEven);
+                } else {
+                    System.out.println("Possible cycle start found at step: " + steps);
+                    cycleStarted = true;
+                }
+            } else {
+                if (cycleStarted) {
+                    cycleStarted = false;
+                }
+            }
+            possiblePositionCounts.add(possiblePositionCount);
+        }
         return visitingPositions.size();
     }
 
@@ -81,47 +114,79 @@ public class Solution {
                 .toList();
     }
 
-    private int countPossiblePositionsAfterSteps2(Set<Position> gardenPositions, Position startingPosition,
-                                                  int stepsToTake) {
+    private int countPossiblePositionsAfterSteps2(Set<Position> gardenPositions, Position startingPosition, int stepsToTake) {
         Set<Position> visitingPositions = new HashSet<>();
         visitingPositions.add(startingPosition);
 
+//        Set<Integer> possiblePositionCounts = new HashSet<>();
+//        boolean cycleStarted = false;
+//        Map<Boolean, Integer> results = new HashMap<>();
+//
         Map<Position, List<Position>> adjacentPositionsOfPosition = new HashMap<>();
 
         int steps = 0;
         while (steps < stepsToTake) {
             Set<Position> newVisitingPositions = new HashSet<>();
-
             for (Position visitingPosition : visitingPositions) {
-                List<Position> adjacentGardenPositions;
-                if (adjacentPositionsOfPosition.containsKey(visitingPosition)) {
-                    adjacentGardenPositions = adjacentPositionsOfPosition.get(visitingPosition);
-                } else {
-                    adjacentGardenPositions =
-                            getAdjacentGardenPositions2(gardenPositions, visitingPosition);
+                List<Position> adjacentGardenPositions = adjacentPositionsOfPosition.get(visitingPosition);
+                if (adjacentGardenPositions == null) {
+                    adjacentGardenPositions = getAdjacentGardenPositions2(gardenPositions, visitingPosition);
                     adjacentPositionsOfPosition.put(visitingPosition, adjacentGardenPositions);
-//                    List<Position> positionsOutsideInitial = adjacentGardenPositions.stream()
-//                            .filter(pos -> isOutsideInitialGarden(pos, rowCount, columnCount))
-//                            .toList();
-//                    if (!positionsOutsideInitial.isEmpty()) {
-//                        System.out.println("Positions outside initial garden: " + positionsOutsideInitial);
-//                    }
                 }
-
                 newVisitingPositions.addAll(adjacentGardenPositions);
             }
+//            Set<Position> positionsOutsideInitial = newVisitingPositions.stream()
+//                    .filter(this::isOutsideInitialGarden)
+//                    .collect(Collectors.toSet());
 
+//            for (Position position : positionsOutsideInitial) {
+//                int i = countPossiblePositionsAfterSteps2(gardenPositions, getProjectedPosition(position), stepsToTake - steps - 1);
+//                otherFarmResults += i;
+//            }
+//            newVisitingPositions.removeAll(positionsOutsideInitial);
+
+            int increaseInStep = newVisitingPositions.size() - visitingPositions.size();
             visitingPositions = newVisitingPositions;
             steps++;
 
-            if (steps % 100 == 0) {
-                System.out.println("Current steps: " + steps);
-                System.out.println("Current possible positions: " + visitingPositions.size());
-            }
-        }
-//        System.out.println(adjacentPositionsOfPosition);
+            int possiblePositionCount = visitingPositions.size();
+            System.out.println("Possible positions after " + steps + " steps:" + possiblePositionCount + " Increase in step: " + increaseInStep);
 
-//        System.out.println("Possible positions: " + visitingPositions);
+//            if (steps % 100 == 0) {
+//                System.out.println("Possible positions after " + steps + " steps:" + possiblePositionCount);
+//            }
+//
+//            // cycle detection
+//            if (possiblePositionCounts.contains(possiblePositionCount)) {
+//                // cycle starts
+//                boolean isEven = steps % 2 == 0;
+//                results.put(isEven, possiblePositionCount);
+//                if (cycleStarted) {
+//                    // have both results in the map now
+//                    System.out.println("Cycle end found, results for isEven: " + results);
+//                    return results.get(isEven) + otherFarmResults;
+//                } else {
+//                    System.out.println("Possible cycle start found at step: " + steps);
+//                    cycleStarted = true;
+//                }
+//            } else {
+//                if (cycleStarted) {
+//                    cycleStarted = false;
+//                }
+//            }
+//            possiblePositionCounts.add(possiblePositionCount);
+
+//            if (!positionsOutsideInitial.isEmpty()) {
+//                System.out.println("Step " + steps + ": Positions outside initial garden: " + positionsOutsideInitial);
+//
+//                List<Position> projectedPositions = positionsOutsideInitial.stream()
+//                        .map(this::getProjectedPosition)
+//                        .toList();
+//
+//                System.out.println("Step " + steps + ": Their projections: " + projectedPositions);
+//            }
+//            printVisitingPositions(newVisitingPositions, gardenPositions);
+        }
         return visitingPositions.size();
     }
 
@@ -151,6 +216,37 @@ public class Solution {
         int projectedColumn = Math.floorMod(position.column, COLUMN_COUNT);
         return new Position(projectedRow, projectedColumn);
     }
+
+//    private void printVisitingPositions(Set<Position> newVisitingPositions, Set<Position> gardenPositions) {
+//        int minRow = Integer.MAX_VALUE;
+//        int maxRow = Integer.MIN_VALUE;
+//        int minColumn = Integer.MAX_VALUE;
+//        int maxColumn = Integer.MIN_VALUE;
+//
+//        for (Position newVisitingPosition : newVisitingPositions) {
+//            minRow = Math.min(minRow, newVisitingPosition.row);
+//            maxRow = Math.max(maxRow, newVisitingPosition.row);
+//            minColumn = Math.min(minColumn, newVisitingPosition.column);
+//            maxColumn = Math.max(maxColumn, newVisitingPosition.column);
+//        }
+//
+//        for (int row = minRow; row <= maxRow; row++) {
+//            for (int column = minColumn; column <= maxColumn; column++) {
+//                Position position = new Position(row, column);
+//                if (isOutsideInitialGarden(position)) {
+//                    position = getProjectedPosition(position);
+//                }
+//                char charToPrint = '#';
+//                if (newVisitingPositions.contains(position)) {
+//                    charToPrint = 'O';
+//                } else if (gardenPositions.contains(position)) {
+//                    charToPrint = '.';
+//                }
+//                System.out.print(charToPrint);
+//            }
+//            System.out.println();
+//        }
+//    }
 
     public static void main(String[] args) throws IOException {
         Solution solution = new Solution();
